64a64f5f262f5fc21a55c700d17ab5e56a326b9b
anasfajar-pratama <pratamaanasfajar@gmail.com>
2025-11-22 05:47:29 +0700

    landingpage static

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\RuanganController;
 use App\Http\Controllers\Auth\StudentRegisterController;
 use App\Http\Controllers\EkycController;
 use App\Http\Controllers\Admin\EkycAdminController;
+use App\Http\Controllers\LandingController;
 
 
 
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/', [LandingController::class, 'index'])->name('landing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
